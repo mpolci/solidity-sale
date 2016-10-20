c20073e920c382063fb7045b1209e3e49a0695d2
Marco Polci <mpolci@gmail.com>
2016-10-20 17:27:09 +0200

    Add basic web interface

---
+++ app/javascripts/app.js
@@ -0,0 +1,94 @@
+angular.module('tokenSaleApp', [])
+.controller('TokenSaleController', function ($scope, $q) {
+  var self = this
+  var DECIMALPLACES = 15
+  angular.extend(this, {
+    promoters: [],
+    saleAddress: null,
+    totalTokensSold: 0,
+    maxTokens: 0,
+    tokenPrice: 0,
+    saleEnded: false,
+
+    selectedAddress: null,
+    balanceOfSelected: 0,
+    updateSelectedBalance: updateSelectedBalance,
+  })
+  var sale = Sale.deployed()
+  self.saleAddress = sale.address;
+
+  $q.all([
+    updatePromoters(),
+    updateTotalTokensSold(),
+    updateSaleEnded(),
+    updateTokenData()
+  ]).catch(error)
+
+  // catch event of tokens sold
+  var eventListener = sale.TokensSold(function (err, event) {
+    if (err) return error(err)
+    console.log('sold', convertTokenNumber(event.args.tokens), 'tokens to:', event.args.buyer)
+    $q.all([
+      updateTotalTokensSold(),
+      updateSaleEnded(),
+    ]).catch(error)
+    if (self.selectedAddress) updateSelectedBalance()
+  })
+
+  $scope.$on('$destroy', function () {
+    eventListener.stopWatching()
+  })
+
+  /************************/
+
+  function error (err) {
+    console.error(err)
+    alert(err.message || err)
+  }
+
+  function convertTokenNumber (value) {
+    return value.shift(-DECIMALPLACES).toNumber()
+  }
+
+  function updatePromoters () {
+    return sale.getPromoters()
+      .then(function (value) {
+        self.promoters = value
+      })
+  }
+
+  function updateTotalTokensSold () {
+    return sale.soldTokens()
+      .then(function (value) {
+        self.totalTokensSold = convertTokenNumber(value)
+      })
+  }
+
+  function updateSaleEnded () {
+    return sale.saleEnded()
+      .then(function (value) {
+        self.saleEnded = value
+      })
+  }
+
+  function updateTokenData () {
+    return $q.all([
+      sale.maxTokens(),
+      sale.tokenPrice(),
+    ]).then(function (values) {
+      self.maxTokens = convertTokenNumber(values[0])
+      self.tokenPrice = web3.fromWei(values[1].shift(DECIMALPLACES)).toNumber()
+    })
+  }
+
+  function updateSelectedBalance () {
+    $q.when(sale.balanceOf(self.selectedAddress)).then(function (value) {
+      self.balanceOfSelected = convertTokenNumber(value)
+    }).catch(function(err) {
+      self.balanceOfSelected = null
+      error(err)
+    })
+  }
+})
+
+/**************************** Tools to create transactions for testing ****************************/
---
+++ app/javascripts/transaction.controller.js
@@ -0,0 +1,62 @@
+angular.module('tokenSaleApp')
+.controller('TransactionController', function ($scope) {
+  var self = this
+  angular.extend(this, {
+    accounts: [],
+    selectedAccount: '',
+    selectedBalance: null,
+    sendAmountInput: null,
+    destinationAddressInput: null,
+    message: null,
+
+    sendTransaction: sendTransaction,
+    canSendTransaction: canSendTransaction,
+  })
+
+  web3.eth.getAccounts(function (err, accounts) {
+    if (err) return console.error(err)
+    self.accounts = accounts
+    $scope.$apply()
+  })
+  $scope.$watch(function () {
+    return self.selectedAccount
+  }, refreshSelectedBalance)
+
+  /*********************************************************/
+
+  function refreshSelectedBalance () {
+    if (!self.selectedAccount) return
+    web3.eth.getBalance(self.selectedAccount, function (err, value) {
+      if (err) return console.error(err)
+      self.selectedBalance = web3.fromWei(value).toNumber()
+      $scope.$apply()
+    })
+  }
+
+  function sendTransaction () {
+    web3.eth.sendTransaction({
+      from: self.selectedAccount,
+      to: self.destinationAddressInput,
+      value: web3.toWei(self.sendAmountInput),
+      gas: 500000
+    }, function (err, txid) {
+      if (err) {
+        console.error(err)
+        self.message = err.toString()
+      } else {
+        var msg = 'new transaction: ' + txid
+        console.log(msg)
+        self.message = msg
+      }
+      $scope.$apply()
+    })
+  }
+
+  function canSendTransaction () {
+    function check (val) {
+      return val && angular.isString(val) && val.length > 0
+    }
+    return check(self.selectedAccount) && check(self.destinationAddressInput) && check(self.sendAmountInput)
+  }
+
+})

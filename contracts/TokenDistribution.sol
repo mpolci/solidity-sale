c1f1119c3040d5645e434672abbcb894a8347185
Marco Polci <mpolci@gmail.com>
2016-10-24 10:28:51 +0200

    Update TokenDistribution contract to use token.transferFrom()

--- contracts/TokenDistribution.sol
+++ contracts/TokenDistribution.sol
@@ -7,6 +7,7 @@ import "owned.sol";
 contract TokenDistribution is owned {
   SaleInterface public sale;
   ERC20 public token;
+  address holder;  // the address owning the tokens to be distributed, it should approve a transfer of a number of tokens equals to the sale
   bool public distributionStarted = false;
   uint public given = 0;
 
@@ -14,9 +15,10 @@ contract TokenDistribution is owned {
 
   event TokenGiven(address recipient, uint quantity);
 
-  function TokenDistribution(SaleInterface _sale, ERC20 _token) {
+  function TokenDistribution(SaleInterface _sale, ERC20 _token, address _holder) {
     sale = _sale;
     token = _token;
+    holder = _holder;
   }
 
   function withdraw() {
@@ -26,7 +28,7 @@ contract TokenDistribution is owned {
   function giveTo(address recipient) {
     if (!distributionStarted) {
       if (!sale.saleEnded()) throw;
-      if (token.balanceOf(this) != sale.soldTokens()) throw;
+      if (token.allowance(holder, this) != sale.soldTokens()) throw;
       distributionStarted = true;
     }
 
@@ -34,7 +36,7 @@ contract TokenDistribution is owned {
     if (qty > 0) {
       givenTo[recipient] += qty;
       given += qty;
-      if (!token.transfer(recipient, qty)) throw;
+      if (!token.transferFrom(holder, recipient, qty)) throw;
       TokenGiven(recipient, qty);
     }
   }
